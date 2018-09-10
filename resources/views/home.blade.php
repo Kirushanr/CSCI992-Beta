2991d2cf23e1ca45e9d6bd7771af42b4448f8531
kirushan <kirushan22@gmail.com>
2018-09-10 23:14:56 +1000

    Updated Explore section
    added custom images ,for each type under explore section,
    fixed HTML syntax errors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts.master')
-@section('title')
-Welcome to BuynSell Beta
+@extends('layouts.master') 
+@section('title') Welcome to BuynSell Beta
 @endsection
+ 
 @section('style')
 <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-@endsection 
+@endsection
+ 
 @section('content')
 
 <header class="jumbotron jumbotron-fluid" style="min-height: 100%!">
@@ -15,60 +16,59 @@ Welcome to BuynSell Beta
             <form action="/search" method="GET">
                 <div class="row justify-content-center">
                     <div class="col-md-6">
-                            <div class="input-group mb-3">
-                                    <input type="text" class="form-control form-control-lg" placeholder="Search for">
-                                    <div class="input-group-append">
-                                      <button class="btn btn-primary" type="submit" id="button-addon2">Search</button>
-                                    </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control form-control-lg" placeholder="Search for">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit" id="button-addon2">Search</button>
                             </div>
+                        </div>
                     </div>
 
                 </div>
             </form>
         </div>
-                 
-    </div> <!-- container-->
 
-</header><!--Jumbotran-->  
-<section>
-    <div class="container">
-        <div class="container">
-            <h3 class="font-weight-light"> Explore BuynSell </h1>
-        </div>
-        <div class="row mt-3 justify-content-around py-4">
-            
-        <div class="col-md-4 mb-3">
-                <div class="card">
-                        <img class="card-img-top" src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and 
-                              make up the bulk of the cards content.</p>
-                        </div>
-                </div>
-        </div>
-        <div class="col-md-4 mb-3">
-            <div class="card">
-                    <div class="card-header bg-dark text-white">Header</div>
-                    <img class="card-img-top" src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap">
-            </div>
     </div>
+    <!-- container-->
 
-        <div class="col-md-4 mb-3">
-                    <div class="card">
-                            <img class="card-img-top" src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap">
-                            <div class="card-body">
-                              <p class="card-text">Some quick example text to build on the card title and 
-                                  make up the bulk of the cards content.</p>
-                            </div>
-                    </div>
-            </div>
-            
-           
+</header>
+<!--Jumbotran-->
+<div class="container">
+    <div class="container">
+        <h3 class="font-weight-light"> Explore BuynSell </h3>
+    </div>
+    <div class="row mt-3 justify-content-center py-4">
+
+        <div class="col-md-4 mb-3 explore">
+            <a href="">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">Books</div>
+                    <img class="card-img-top" src="https://images.pexels.com/photos/775998/pexels-photo-775998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                        alt="Card image cap">
+                </div>
+            </a>
         </div>
-   </div>
-</container>
 
-</section> 
+        <div class="col-md-4 mb-3 explore">
+            <a href="">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">Electronics</div>
+                    <img class="card-img-top" src="https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                        alt="Card image cap">
+                </div>
+            </a>
+        </div>
 
+        <div class="col-md-4 mb-3 explore">
+            <a href="">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">Kitchenware</div>
+                    <img class="card-img-top" src="https://images.pexels.com/photos/350417/pexels-photo-350417.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                        alt="Card image cap">
+                </div>
+            </a>
+        </div>
 
-@endsection 
+    </div>
+</div>
+@endsection
