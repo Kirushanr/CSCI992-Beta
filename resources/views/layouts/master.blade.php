3fd47f2b8e889e55f7c096eec03ceafa9539c9ca
kirushan <kirushan22@gmail.com>
2018-09-10 23:11:18 +1000

    Formatted Blade syntax, Modification Navbar
    Added dropdown menu (if the user has logged in)
    Fixed blade, html syntaxes
    Added callout button

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,5 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,7 +10,7 @@
 
     <title>{{ config('app.name') }}</title>
 
-   
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -17,43 +18,71 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <nav class="navbar navbar-dark navbar-expand-sm" style="background-color: #0c2340;">
-            <div class="container">
-                <a href="{{ route('home') }}" class="navbar-brand">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-sm" style="background-color: #0c2340;">
+        <div class="container">
+            <a href="{{ route('home') }}" class="navbar-brand">
                     <img src="{{asset('images/logo.svg')}}" width="120" height="30" alt="">
                 </a>
-    
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navToggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navToggler" aria-controls="navbarTogglerDemo01"
+                aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
-    
-                <div class="collapse navbar-collapse" id="navToggler">
-                    <div class="navbar-nav ml-auto">
-                        <a class="nav-link" href="{{ route('post-ad') }}">Sell Your Stuff +</a>
-                        <a class="nav-link" href="#services">Help</a>
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </div><!-- navbar-nav -->
+
+            <div class="collapse navbar-collapse" id="navToggler">
+                <div class="navbar-nav ml-auto">
+
+                    <a class="nav-link text-white rounded-0 sell" href="{{ route('post-ad') }}">Sell Your Stuff +</a>
+                    <a class="nav-link text-white" href="#services">Help</a> @guest
+                    <a class="nav-link text-white" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link text-white" href="{{ route('register') }}">Register</a> @else
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              My Account
+                            </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Dashboard</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                 </div>
-                <!--collapse-->
-    
-    
-            </div><!-- container -->
-        </nav> <!-- navbar-->
+                </li>
+                <!-- Dropdown-->
 
-            @yield('content')            
-
-
-
-            {{--<footer class="footer">
-                <div class="container">
-                  <span class="text-muted">Created by BuynSell </span>
-                </div>
-              </footer>--}}
-            <script src="{{asset('js/app.js')}}"></script>
-            @yield('script')
+                @endguest
+            </div>
+            <!-- navbar-nav -->
         </div>
-    </body>
+        <!--collapse-->
+
+
+        </div>
+        <!-- container -->
+    </nav>
+    <!-- navbar-->
+
+    @yield('content') {{--
+
+
+
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Created by BuynSell </span>
+        </div>
+    </footer>--}}
+
+
+    <script src="{{asset('js/app.js')}}"></script>
+    @yield('script')
+</body>
+
 </html>
