7bbd4b31ef8710c72d53db8ac7569c93a135ce34
1world0x00 <xl561@uowmail.edu.au>
2018-10-10 19:05:25 +1100

    This rating system uses laravel-rateable plugin.

---
+++ app/Http/Controllers/ReviewPostController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1world0x00
+ * Date: 10/10/18
+ * Time: 6:24 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class ReviewPostController
+{
+
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1world0x00
+ * Date: 10/10/18
+ * Time: 6:31 PM
+ */
