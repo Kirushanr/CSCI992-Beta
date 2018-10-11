031e0cf7bf9d495d7c03786da3ad8cf444e4485a
kirushan <kirushan22@gmail.com>
2018-10-12 02:14:53 +1100

    New routes added, names modified
    New routes added for posting advert, and viewing an advert.

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,18 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-
-
-
 Route::get('/dashboard',function(){
     return view('admin.dashboard');
 });
 
 //Route to GET the home/main page of post advert
-Route::get('/post/ad','AdvertsController@getAdvertHome')->name('post-ad');
+Route::get('/post/ad','AdvertsController@index')->name('post-ad');
 
 //Route to GET the post advert page based on the type parameter
 Route::get('/post/ad/{type}','AdvertsController@getAdvert')->name('post-ad-type.show');
 
-
 Route::get('/search',SearchAdvertController::class);
+
+Route::post('/post/ad','AdvertsController@postAdvert')->name('store-ad');
+
+Route::get('/advert/{id}','AdvertsController@viewAdvert')->name('view-advert');
