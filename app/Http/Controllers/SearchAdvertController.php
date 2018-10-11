ac233dcd8e8cfb6829c207d208aab12b59ca0fe9
kirushan <kirushan22@gmail.com>
2018-10-12 02:03:55 +1100

    Search function
    Search function View, and Controller.
    Functionaltity to sort adverts, view adverts, group adverts by advert category

--- app/Http/Controllers/SearchAdvertController.php
+++ app/Http/Controllers/SearchAdvertController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use App\Advert;
+use App\Book;
+use App\Electronics;
 
 class SearchAdvertController extends Controller
 {
@@ -14,18 +19,185 @@ class SearchAdvertController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
-        $category = $request->get('category');
-       	$this->getElectronicsAdverts();
 
+
+        $category = $request->get('category');
+        $sort = $request->get('sort');
+
+
+        /**
+         * 0=> All Categories
+         * 1=> Books
+         * 2=> Electronics
+         * 3=> Essentials
+         */
+        $os = array(0, 1, 2, 3);
+
+
+        $adverts = array();
+
+
+        $sort_cateogry = array('newest', 'pricelowhigh', 'pricehighlow', 'name');
+        $sort_by = (in_array($sort, $sort_cateogry) ? $sort : 'newest');
+      
+        //Check if the category is available
+        if (in_array($category, $os)) {
+
+            switch ($category) {
+                case 0:
+                    $adverts = $this->getAllAdverts($sort_by);
+                    break;
+                case 1:
+                    $adverts = $this->getBookAdverts($sort_by);
+                    break;
+                case 2:
+                    $adverts = $this->getElectronicsAdverts($sort_by);
+                    break;
+                case 3:
+                    $adverts = $this->getEssentials($sort_by);
+                    break;
+            }
+        }
+        
+        return view("search.search")->with('adverts', $adverts);
     }
 
-    public function getElectronicsAdverts()
-    {   
-        $data=DB::table('adverts')->join('electronics','adverts.id','=','electronics.advert_id')->select('adverts.title','adverts.description','adverts.price','electronics.model','electronics.type')->get();
-        echo "<pre>";
-        print_r($data);
+    public function getElectronicsAdverts($param)
+    {
+        $DB = DB::table('adverts')
+            ->join('electronics', 'adverts.id', '=', 'electronics.advert_id')
+            ->select(
+                'adverts.id',
+                'adverts.title',
+                'adverts.description',
+                'image_url_1',
+                'image_url_2',
+                'image_url_3',
+                'adverts.price',
+                'adverts.advert_type',
+                'adverts.created_at'
+            )->where([
+                ['visibility', '=', true],
+                ['expired', '=', false],
+            ]);
 
+        if ($param == "newest") {
+            $DB = $DB->orderBy('adverts.created_at', 'desc');
+        } else if ($param == "pricelowhigh") {
+            $DB = $DB->orderBy('adverts.price', 'asc');
+        } else if ($param == "pricehighlow") {
+            $DB = $DB->orderBy('adverts.price', 'desc');
+        } else {
+            $DB = $DB->orderBy('adverts.title', 'asc');
+        }
+
+
+        $data = $DB->get();
+
+        return $data;
+    }
+
+
+    public function getAllAdverts($param)
+    {
+        $DB = DB::table('adverts')->select(
+            'adverts.id',
+            'adverts.title',
+            'adverts.description',
+            'image_url_1',
+            'image_url_2',
+            'image_url_3',
+            'adverts.price',
+            'adverts.advert_type',
+            'adverts.created_at'
+        )->where([
+            ['visibility', '=', true],
+            ['expired', '=', false],
+        ]);
+
+        if ($param == "newest") {
+            $DB = $DB->orderBy('adverts.created_at', 'desc');
+        } else if ($param == "pricelowhigh") {
+            $DB = $DB->orderBy('adverts.price', 'asc');
+        } else if ($param == "pricehighlow") {
+            $DB = $DB->orderBy('adverts.price', 'desc');
+        } else {
+            $DB = $DB->orderBy('adverts.title', 'asc');
+        }
+
+
+        $data = $DB->get();
+        return $data;
+    }
+
+    public function getBookAdverts($param)
+    {
+        $DB = DB::table('adverts')
+            ->join('books', 'adverts.id', '=', 'books.advert_id')
+            ->select(
+                'adverts.id',
+                'adverts.title',
+                'adverts.description',
+                'image_url_1',
+                'image_url_2',
+                'image_url_3',
+                'adverts.price',
+                'adverts.advert_type',
+                'adverts.created_at'
+            )->where([
+                ['adverts.visibility', '=', true],
+                ['adverts.expired', '=', false],
+            ]);
+
+        if ($param == "newest") {
+            $DB = $DB->orderBy('adverts.created_at', 'desc');
+        } else if ($param == "pricelowhigh") {
+            $DB = $DB->orderBy('adverts.price', 'asc');
+        } else if ($param == "pricehighlow") {
+            $DB = $DB->orderBy('adverts.price', 'desc');
+        } else {
+            $DB = $DB->orderBy('adverts.title', 'asc');
+        }
+
+
+        $data = $DB->get();
+
+
+
+        return $data;
+    }
+
+    public function getEssentials($param)
+    {
+        $DB = DB::table('adverts')->select(
+            'adverts.id',
+            'adverts.title',
+            'adverts.description',
+            'image_url_1',
+            'image_url_2',
+            'image_url_3',
+            'adverts.price',
+            'adverts.advert_type',
+            'adverts.created_at'
+        )->where([
+            ['visibility', '=', true],
+            ['expired', '=', false],
+            ['advert_type','=','essentials']
+        ]);
+
+        if ($param == "newest") {
+            $DB = $DB->orderBy('adverts.created_at', 'desc');
+        } else if ($param == "pricelowhigh") {
+            $DB = $DB->orderBy('adverts.price', 'asc');
+        } else if ($param == "pricehighlow") {
+            $DB = $DB->orderBy('adverts.price', 'desc');
+        } else {
+            $DB = $DB->orderBy('adverts.title', 'asc');
+        }
+
+
+        $data = $DB->get();
+        return $data;
     }
 }
 
--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -13,7 +13,8 @@
 
                 <div class="row mt-2">
                     <div class="input-group">
-                        <input type="text" class="form-control" aria-label="Text input with dropdown button" value="{{ app('request')->input('query') }}" name="query">
+                        <input type="text" class="form-control" aria-label="Text input with dropdown button" value="{{ app('request')->input('query') }}"
+                            name="query">
                         <div class="input-group-append">
                             <select class="custom-select" name="category">
                                 <option value="0" >All Categories</option>
@@ -32,6 +33,35 @@
             </form>
         </div>
     </div>
+    <div class="results mt-5">
+        <div class="container">
+            <div class="row">
+                @forelse ($adverts as $advert)
+                <div class="col-md-4 mb-2">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="{{URL::asset('storage/' . $advert->image_url_1)}}" alt="Card image cap" style="height: 150px; width: 100%; display: block;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$advert->title}}</h5>
+                            <p class="card-text">{{ str_limit($advert->description, $limit = 150, $end = '...') }}</p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{ URL::route('view-advert', $advert->id ) }}"  class="btn btn-primary">View</a>
+                                </div
+                                <small class="card-title">${{$advert->price}}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @empty
+                <div class="col-md-12">
+                    <p class="alert alert-danger font-weight-bold mt-2 text-center">No Results Found</p>
+                </div>
+
+                @endforelse
+            </div>
+        </div>
+    </div>
 
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -45,10 +75,10 @@
                 <div class="modal-body text-center">
                     <ul class="list-unstyled">
 
-                        <li><a href="#">Newest</a></li>
-                        <li><a href="#">Price Low to High</a></li>
-                        <li><a href="#">Price High to Low</a></li>
-                        <li><a href="#">Name</a></li>
+                        <li><a href="{{Request::fullUrlWithQuery(['sort' => 'newest'])}}">Newest</a></li>
+                        <li><a href="{{Request::fullUrlWithQuery(['sort' => 'pricelowhigh'])}}">Price Low to High</a></li>
+                        <li><a href="{{Request::fullUrlWithQuery(['sort' => 'pricehighlow'])}}">Price High to Low</a></li>
+                        <li><a href="{{Request::fullUrlWithQuery(['sort' => 'name'])}}">Name</a></li>
                     </ul>
                 </div>
                 <div class="modal-footer">
