94789e797a21b8a1ce79538c8ecb02b333a80a0b
1world0x00 <xl561@uowmail.edu.au>
2018-10-14 17:55:19 +1100

    This is buyers and sellers review version. This system use Laravel-rateable plugin. Thus, before use it, plese composer require this plugin.

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1world0x00
+ * Date: 14/10/18
+ * Time: 5:39 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class ReviewController
+{
+
+}
---
+++ resources/views/ratesShow.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1world0x00
+ * Date: 14/10/18
+ * Time: 5:44 PM
+ */
