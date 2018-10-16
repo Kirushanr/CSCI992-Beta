8122070b6dabb432a8dcdfbe4aaf45deef0d60b2
kirushan <kirushan22@gmail.com>
2018-10-16 17:18:56 +1100

    Updated master.blade.php
    Replaced  Dashboard hyperlink with the route , in the navigation bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -46,7 +46,7 @@
                               My Account
                             </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Dashboard</a>
+                            <a class="dropdown-item" href="{{ route('user-dashboard') }}">Dashboard</a>
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                   document.getElementById('logout-form').submit();">
                                       {{ __('Logout') }}
