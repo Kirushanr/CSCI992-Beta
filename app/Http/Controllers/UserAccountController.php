144c873eeb03c4f696218c74b97f3c46dcf07020
kirushan <kirushan22@gmail.com>
2018-10-16 17:25:40 +1100

    Added UserAccountController
    UserAccountController was created, to manage user dashboard functionality

---
+++ app/Http/Controllers/UserAccountController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class UserAccountController extends Controller
+{
+    public function index(){
+        $user = Auth::user();
+        $adverts =$user->adverts;
+        return view('account.partial.useradverts')->with(compact('adverts'));
+    }
+    
+    public function getWishList(){
+        $user = Auth::user();
+        $adverts =$user->wishlist;   
+        return view("account.partial.userwishlist")->with('adverts', $adverts);
+      
+    }
+}
