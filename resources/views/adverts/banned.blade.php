ba568a80e94f6cd22a619d15a668e9bf425f2be5
kirushan <kirushan22@gmail.com>
2018-10-20 20:21:03 +1100

    Added new views for banned adverts
    Users will be redirected to banned default page, when they click a banned advert

---
+++ resources/views/adverts/banned.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+<div class="container">
+<div class="row justify-content-center mt-5 text-center">
+    <div class="col-md-6">
+        <img src={{asset('images/banned.png')}} style="heigh:200px;width:200px">
+        <h3 class="display-5" style="height:100px;">The advert you'are trying to view is banned for violating BuynSell policies.</h3>
+     </div>
+</div>
+</div>
+@endsection
