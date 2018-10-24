052dfa7aa23678d21f55b4fa60166f1ed7b65f9f
kirushan <kirushan22@gmail.com>
2018-10-24 11:06:50 +1100

    Fixed responsive layout issue in administrator dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,155 +3,72 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
             <link href="{{ asset('css/app.css') }}" rel="stylesheet">
             <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-            <style>
-                    body {
-                        font-size: .875rem;
-                      }
-                      
-                      .feather {
-                        width: 16px;
-                        height: 16px;
-                        vertical-align: text-bottom;
-                      }
-                      
-                      /*
-                       * Sidebar
-                       */
-                      
-                      .sidebar {
-                        position: fixed;
-                        top: 0;
-                        bottom: 0;
-                        left: 0;
-                        z-index: 100; /* Behind the navbar */
-                        padding: 48px 0 0; /* Height of navbar */
-                        box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-                      }
-                      
-                      .sidebar-sticky {
-                        position: relative;
-                        top: 0;
-                        height: calc(100vh - 48px);
-                        padding-top: .5rem;
-                        overflow-x: hidden;
-                        overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-                      }
-                      
-                      @supports ((position: -webkit-sticky) or (position: sticky)) {
-                        .sidebar-sticky {
-                          position: -webkit-sticky;
-                          position: sticky;
-                        }
-                      }
-                      
-                      .sidebar .nav-link {
-                        font-weight: 500;
-                        color: #333;
-                      }
-                      
-                      .sidebar .nav-link .feather {
-                        margin-right: 4px;
-                        color: #999;
-                      }
-                      
-                      .sidebar .nav-link.active {
-                        color: #007bff;
-                      }
-                      
-                      .sidebar .nav-link:hover .feather,
-                      .sidebar .nav-link.active .feather {
-                        color: inherit;
-                      }
-                      
-                      .sidebar-heading {
-                        font-size: .75rem;
-                        text-transform: uppercase;
-                      }
-                      
-                      /*
-                       * Content
-                       */
-                      
-                      [role="main"] {
-                        padding-top: 48px; /* Space for fixed navbar */
-                      }
-                      
-                      /*
-                       * Navbar
-                       */
-                      
-                      .navbar-brand {
-                        padding-top: .75rem;
-                        padding-bottom: .75rem;
-                        font-size: 1rem;
-                        background-color: rgba(0, 0, 0, .25);
-                        box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-                      }
-                      
-                      .navbar .form-control {
-                        padding: .75rem 1rem;
-                        border-width: 0;
-                        border-radius: 0;
-                      }
-                      
-                      .form-control-dark {
-                        color: #fff;
-                        background-color: rgba(255, 255, 255, .1);
-                        border-color: rgba(255, 255, 255, .1);
-                      }
-                      
-                      .form-control-dark:focus {
-                        border-color: transparent;
-                        box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
-                      }
-                      
-            </style>
+
     </head>
     <body>
-            <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BuynSell</a>
-                    <ul class="navbar-nav px-3">
-                      <li class="nav-item text-nowrap">
-                        <a class="nav-link" href="#">Sign out</a>
-                      </li>
-                    </ul>
-                  </nav>
-              
-                  <div class="container-fluid">
-                    <div class="row">
-                      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                        <div class="sidebar-sticky">
-                          <ul class="nav flex-column">
-                            <li class="nav-item">
-                            
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="{{route('reported.adverts')}}">
-                                <span data-feather="file"></span>
-                                Reported Adverts
-                              </a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="{{route('banned.adverts')}}">
-                                <span data-feather="shopping-cart"></span>
-                                Banned Adverts
-                              </a>
-                            </li>
-                           
-                          </ul>
-              
+        <nav class="navbar navbar-dark navbar-expand-sm" style="background-color: #0c2340;">
+            <div class="container">
+                <a href="{{ route('home') }}" class="navbar-brand">
+                        <img src="{{asset('images/logo.svg')}}" width="120" height="30" alt="">
+                    </a>
+    
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navToggler" aria-controls="navbarTogglerDemo01"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+    
+                <div class="collapse navbar-collapse" id="navToggler">
+                    <div class="navbar-nav ml-auto">
+    
+                        <a class="nav-link text-white" href="{{route('reported.adverts')}}">Reported adverts</a>
                          
-                          
-                        </div>
-                      </nav>
-              
-                      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                          @yield('content')          
-                                      
-              
-                      </main>
+                        @guest
+                        <a class="nav-link text-white" href="{{ route('login') }}">Login</a>
+                        <a class="nav-link text-white" href="{{ route('register') }}">Register</a> 
+                        @else
+                       
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  My Account
+                                </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="{{ route('user-dashboard') }}">My User Dashboard</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                      document.getElementById('logout-form').submit();">
+                                          {{ __('Logout') }}
+                                      </a>
+                            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                     </div>
-                  </div>
+                    </li>
+                    <!-- Dropdown-->
+    
+                    @endguest
+                </div>
+                <!-- navbar-nav -->
+            </div>
+            <!--collapse-->
+    
+    
+            </div>
+            <!-- container -->
+        </nav>           
+        <div class="container-fluid">
+          <div class="row">
+
+
+              <main role="main" class="col-md-12 col-sm-12 col-lg-12 px-4">
+                  @yield('content')                       
+              </main>
+
+          </div>
+           
+        </div>
+                      
+                    
+                  
                   <script src="{{asset('js/app.js')}}"></script>
                   @yield('script')
     </body>
